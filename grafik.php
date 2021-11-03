aa7c38e6f04db346959697ec003006b477076a31
mirzaq19 <mirzaqarjap@gmail.com>
2021-11-03 17:32:07 +0700

    update: navbar to component

--- grafik.php
+++ grafik.php
@@ -16,27 +16,7 @@
     <title>Grafik</title>
   </head>
   <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-success">
-    <div class="container">
-      <a class="navbar-brand" href="#">Pemrograman Web</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="./grafik.php">Grafik</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./viewDB.php">View DB</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./tabel.php">Tabel</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+    <?php include './navcomp.php'?>
     <div class="container my-5">
       <div class="row">
         <div class="col-6">
